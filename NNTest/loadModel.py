2463bc556eab340d7aec3ecc4152a5b8dabba892
xindaqi <xdq101@qq.com>
2018-12-12 20:02:12 +0800

    update for load model get graph variables

--- NNTest/loadModel.py
+++ NNTest/loadModel.py
@@ -37,11 +37,9 @@ def loadModel():
 
 		plt.show()
 		plt.savefig('images/loadModelPredict.png', format='png')
-
-
-
-		# print("Load Weights_1 from model: {}".format(sess.run('weights_1:0')))
-		# print("Load Weights_2 from model: {}".format(sess.run('weights_2:0')))
-		# print("Load Biases_1 from model: {}".format(sess.run('biases_1:0')))
-		# print("Load Biases_2 from model: {}".format(sess.run('biases_2:0')))
+		获取模型变量
+		print("Load Weights_1 from model: {}".format(sess.run('weights_1:0')))
+		print("Load Weights_2 from model: {}".format(sess.run('weights_2:0')))
+		print("Load Biases_1 from model: {}".format(sess.run('biases_1:0')))
+		print("Load Biases_2 from model: {}".format(sess.run('biases_2:0')))
 loadModel()
